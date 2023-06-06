af525f03f93b32aa8d500ec0942f55bbc56f6667
Deepak-134 <127676880+Deepak-134@users.noreply.github.com>
2023-06-06 20:30:09 +0530

    Create SQL_&_Pyspark.py

---
+++ SQL_&_Pyspark.py
@@ -0,0 +1,19 @@
+1) SELECT * FROM COUNTRY
+   WHERE AREA >= 3000000 OR POPULATION >= 25000000;
+   
+   df.filter((df['area'] >= 3000000) | (df['population'] >= 25000000))
+  
+2) SELECT DISTINCT AUTHOR_ID AS ID
+   FROM VIEWS
+   WHERE AUTHOR_ID = VIEWER_ID
+   ORDER BY ID;
+   
+   df.filter(df.author_id == df.viewer_id).select(df.author_id.alias('id')).distinct().orderBy(df.id.asc())
+    
+3) SELECT TWEET_ID
+   FROM TWEETS
+   WHERE CHAR_LENGTH(CONTENT) > 15;
+   
+   df.filter(len(df.content) > 15).select(df.tweet_id)
+
+4) 
